ba3c1cd2e3b51316f34e10fc26819c96fc965d1f
Admin <chris@postfmly.com>
2026-07-11 19:17:55 -0500

    chore: update settings refactor: add Block (TODO)

---
+++ GLJ2026/Assets/Scripts/Block.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+
+// ? TODO: abstract/inheritance for different Block types?
+
+public class Block : MonoBehaviour
+{
+    // ! NOTE: Assumes surface has Collider attached (MeshCollider/BoxCollider) for Physics.Raycast
+
+    // for UI
+    #pragma warning disable IDE0044
+	[Header("Placement Settings")]
+	[SerializeField] private GameObject prefab;
+	[SerializeField] private LayerMask surface;
+	[SerializeField] private float maxDistance = 50f;
+    #pragma warning restore IDE0044
+
+	private GameObject obj;
+	private new Camera camera;
+
+	private Vector3 position;
+	private Quaternion rotation;
+
+	/*void Start()
+	{
+		camera = Camera.main; // MainCamera
+		StartPlacementMode();
+	}
+
+	void Update()
+	{
+		if (obj)
+		{
+			MovePreviewWithMouse();
+
+			// left mouse
+			if (Input.GetMouseButtonDown(0))
+			{
+				FinalizePlacement();
+			}
+		}
+	}*/
+
+	// preview prefab in world as ghost
+	private void StartPlacementMode()
+	{
+		if (prefab)
+		{
+			obj = Instantiate(prefab);
+
+			//disable colliders on preview to prevent raycast from hitting itself
+			if (obj.TryGetComponent<Collider>(out Collider col))
+			{
+				col.enabled = false;
+			}
+		}
+	}
+
+	private void MovePreviewWithMouse()
+	{
+		// ray from camera to point
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+		if (Physics.Raycast(ray, out RaycastHit hit, maxDistance, surface))
+		{
+			// enable preview mesh
+			obj.SetActive(true);
+
+			// calculate offset so prefab doesn't clip into floor
+			float offset = prefab.GetComponent<Renderer>().bounds.size.y / 2;
+			position = hit.point + (hit.normal * offset);
+
+			// rotate to Up vector matches surface normal
+			rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+			obj.transform.position = position;
+			obj.transform.rotation = rotation;
+		} else
+		{
+			obj.SetActive(false);
+		}
+	}
+
+	private void FinalizePlacement()
+	{
+		// enable collider
+		if (obj.TryGetComponent<Collider>(out Collider col))
+		{
+			col.enabled = true;
+		}
+
+		// release pointer tracking
+		obj = null;
+
+		// place object on surface
+		Instantiate(prefab, position, rotation);
+	}
+
+    private void OnCollision(Collision collision)
+    {
+		// ! NOTE: Assumes object has Player tag
+
+        // check if has tag Player
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
